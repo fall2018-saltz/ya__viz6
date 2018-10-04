05f55a6b1b65d3c0f943003c5d03e06a67019520
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:39:48 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,4 @@
 
-clean_data <- raw_data
 cenFun=function()
 {
 dfStates=read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv") , stringsAsFactors = FALSE)
