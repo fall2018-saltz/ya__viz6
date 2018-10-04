506948f26ca0a704b0da7f4c231e4ee7acd9c756
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:55:56 -0400

    Saving censusRead.R

--- censusRead.R
+++ censusRead.R
@@ -1,7 +1,8 @@
 
 cenFun=function()
 {
-dfStates=rawdatadfStates=dfStates[-53,]
+dfStates=raw_data
+dfStates=dfStates[-53,]
 dfStates=dfStates[-1,]
 dfStates=dfStates[,-c(1,2,3,4)]
 colnames(dfStates)=c("stateName","population","popOver18","percentOver18")
