cd832c3eb16ed5ede1317281ff3ca241fb2ec1b2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:42:19 -0400

    Saving hist1.R

--- hist1.R
+++ hist1.R
@@ -1,13 +1,12 @@
 
 #4)	Create a histogram using GGPLOT for the population and a different histogram for the murder rate
-
+#histogram for population
 #to use ggplots we must use ggplots library
-#import ggplots library
+#ggplots is already installed so just use it
 library("ggplot2")
-#generate a histogram
-#dataset. We are using geom_histogram() function with binwidth attribute to group the data 
-#into bins. Here, we are using 500,000 as binwidth because the population is in millions.
-myPlotPop <- ggplot(mergeDataframe, aes(x=population))
-myPlotPop <- myPlotPop + geom_histogram(binwidth = 500000)
-myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
+#generate a histogram for population
+#aes is used to add the axis here
+popHist=ggplot(mergeDataframe, aes(x=population))
+myPlotPop=myPlotPop + geom_histogram(binwidth = 400000)
+myPlotPop=myPlotPop + ggtitle("Histogram of Population")
 myPlotPop
