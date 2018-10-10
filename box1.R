545c6a1c64e61b13e95ee1f2fb45f88e7cb1b0ae
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:48:10 -0400

    Saving box1.R

--- box1.R
+++ box1.R
@@ -1,1 +1,15 @@
 
+#ggplot2 library has to be used to create ggplots
+library("ggplot2")
+
+myPlotPop2 <- ggplot(mergeDataframe, aes(x=factor(0),y=population))
+myPlotPop2 <- myPlotPop2 + geom_boxplot()
+myPlotPop2 <- myPlotPop2 + ggtitle("Boxplot of Population")
+myPlotPop2
+
+#using ggplot() to generate the boxplot. we also use aes() to visualize the boxplot with 
+#variables on x and y axes.
+myPlotPop3 <- ggplot(mergeDataframe, aes(x=factor(0),y=Murder))
+myPlotPop3 <- myPlotPop3 + geom_boxplot()
+myPlotPop3 <- myPlotPop3 + ggtitle("Boxplot of Murder")
+myPlotPop3
