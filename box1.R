542eb3f8630296a49c0d59b7a9aacef9186b01b5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:56:03 -0400

    Saving box1.R

--- box1.R
+++ box1.R
@@ -3,15 +3,17 @@
 #just use the library as it is already imported
 
 library("ggplot2")
+
+#create a boxplot for population
 #aes are used to add a and y axes
-popBox=ggplot(mergeDataframe, aes(x=factor(0),y=population))
+popBox=ggplot(mergedData, aes(x=factor(0),y=population))
 #geom_boxplot is used to create a boxplot
 popBox=popBox+geom_boxplot()
-popBox=popBox+ggtitle("Boxplot of Population")
+popBox=popBox+ggtitle("Population Boxplot")
 popBox
 
 #generate a Boxplot for Murder
-myPlotPop3 <- ggplot(mergeDataframe, aes(x=factor(0),y=Murder))
-myPlotPop3 <- myPlotPop3 + geom_boxplot()
-myPlotPop3 <- myPlotPop3 + ggtitle("Boxplot of Murder")
-myPlotPop3
+murBox=ggplot(mergedData, aes(x=factor(0),y=Murder))
+murBox=murBox+ geom_boxplot()
+murBox=murBox+ ggtitle("Murder Boxplot")
+murBox
