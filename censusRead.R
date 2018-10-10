756913607d96b57e7b976f0962ee47290dfa8a52
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:04:21 -0400

    Saving censusRead.R

--- censusRead.R
+++ censusRead.R
@@ -7,7 +7,6 @@ dfStates=raw_data
 
 #remove row 53 to remove the last row using the minus symbol
 dfStates=dfStates[-53,]
-View(dfStates)
 #to make only 51 rows available we need to remove one more row and according to the question,
 #remove the 1st row as it is not a state and according to the condition we should have the rows as one per state and the district of Columbia
 dfStates=dfStates[-1,]
@@ -24,8 +23,7 @@ colnames(dfStates)
 #to change the names to new names asked in the question
 colnames(dfStates)=c("stateName","population","popOver18","percentOver18")
 
-#view the dataset after changing the column names
-View(dfStates)
+
 return(dfStates)
 }
 cleandata=cenFun(raw_data)
