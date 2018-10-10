addeddad6a4f9b036aee4b2734659faa1e0f91b6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:22:56 -0400

    Saving murCharts.R

--- murCharts.R
+++ murCharts.R
@@ -1,28 +1,25 @@
 
 #importing library for ggplot2 to generate barcharts
 library("ggplot2")
-#calculating the number of murders per state and then displaying the new subset of mergeDataframe
-#in murdersPerState
-mergeDataframe$murPerState=as.integer(mergedData$population*mergedData$Murder/100000)
-murdersPerState <- mergeDataframe[,c(2,3,4,10)]
-murdersPerState
+#to calculate the number of murders per state
+mergedData$murPerState=as.integer(mergedData$population*mergedData$Murder/100000)
+stateMurders=mergedData[,c(2,3,4,10)]
+stateMurders
 
-#generating a barchart for number of murders per state using ggplot() and aes() to 
-#visualize the graph
-barChart1 <- ggplot(mergeDataframe, aes(x=stateName, y=numMurders))
-barChart1 <- barChart1 + geom_col()
-barChart1 <- barChart1 + ggtitle("Bar chart of murders per state")
-barChart1
+#to create barchart for number of murders per state
+murCount=ggplot(mergedData, aes(x=stateName, y=murPerState))
+murCount=murCount+ geom_col()
+murCount=murCount+ ggtitle("Murders per State")
+murCount
 
-#generating a barchart to rotate text on x-axis using theme() and aes() to visualize the graph
-barChart2 <- ggplot(mergeDataframe, aes(x=stateName, y=numMurders))
-barChart2 <- barChart2 + geom_col()
-barChart2 <- barChart2 + ggtitle("Total Murders")
-barChart2 <- barChart2 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-barChart2
+#to create barchart for number of murders per state while rotating text and adding title
+murCount2=ggplot(mergedData, aes(x=stateName, y=numMurders))
+murCount2=murCount2+ geom_col()
+murCount2=murCount2 + ggtitle("Total Murders")
+murCount2=murCount2+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
+murCount2
 
-#generating a barchart of sorted murder rate using ggplot() and aes() to visualize the graph
-barChart3 <- ggplot(mergeDataframe, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1))
+#to create barchart for murder rate in increasing orderbarChart3 <- ggplot(mergeDataframe, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1))
 barChart3 <- barChart3 + geom_col()
 barChart3 <- barChart3 + ggtitle("Sorted bar chart of murders per state")
 barChart3
