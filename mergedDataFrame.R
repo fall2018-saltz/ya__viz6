009d7ded86d160a4493c31e360c83b1fdb39d899
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:17:51 -0400

    Saving mergedDataFrame.R

--- mergedDataFrame.R
+++ mergedDataFrame.R
@@ -4,5 +4,6 @@ arrests=USArrests
 
 #arrests doesn't contain a proper clumn name for state names but has state names as index
 #so create a column to store the state names suing rownames
+rownames(cleandata)=as.vector(cleandata[,"stateName"})
 
 
